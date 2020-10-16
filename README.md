f2c084740e28b694052526939ed6250eeadb6a55
unknown <kiwwanne@gmail.com>
2020-10-16 14:46:19 -0500

    Finished sub for calculating greatest amoung the summary. Made some formatting changes as well as some function work.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # VBA-challenge
+
+## Background
+This task will be using VBA scripting to analyze real stock market data. Using VBA I will be generating a yearly stock performance summary focused on yearly change, percent change, and total volume.
--- vba-challenge.vb
+++ vba-challenge.vb
@@ -1,13 +1,13 @@
 Sub main():
     
-    Dim price_o As Double
-    Dim price_c As Double
-    Dim yrChange As Double
-    Dim Ticker As String
-    Dim rowCount As Integer
-    Dim vol As Double
-    Dim lastRow As Long
-    Dim ws As Worksheet
+    Dim price_o     As Double
+    Dim price_c     As Double
+    Dim yrChange    As Double
+    Dim Ticker      As String
+    Dim rowCount    As Integer
+    Dim vol         As Double
+    Dim lastRow     As Long
+    Dim ws          As Worksheet
     
     For Each ws In Worksheets
         rowCount = 2
@@ -43,51 +43,65 @@ Sub main():
                 vol = vol + ws.Cells(i, 7).Value
                 
             End If
-    
-            Next i
-            Call greatCal(ws)
-        Next
-    End Sub
-    
-    Sub cellFormats(cnt As Integer, ws As Worksheet):
-        If ws.Range("J" & cnt).Value < 0 Then
             
-            ws.Range("J" & cnt).Interior.ColorIndex = 3
-            
-        Else
-            
-            ws.Range("J" & cnt).Interior.ColorIndex = 4
-            
-        End If
-        ws.Range("I:I", "L:L").BorderAround LineStyle:=xlContinuous, Weight:=xlThin
-        ws.Range("I:I", "L:L").EntireColumn.AutoFit
-        ws.Range("I1:L1").Interior.ColorIndex = 36
-    End Sub
-    
-    Sub divZero(ws As Worksheet, rowCount As Integer, yrChange As Double, open_price As Double)
+        Next i
+        Call greatCal(ws, lastRow)
+    Next
+End Sub
+
+Sub cellFormats(cnt As Integer, ws As Worksheet):
+    If ws.Range("J" & cnt).Value < 0 Then
         
-        If open_price = 0 Then
-            
-            ws.Range("K" & rowCount).Value = 0
-            
-        Else
-            
-            ws.Range("K" & rowCount).Value = Format(yrChange / open_price, "Percent")
-            
-        End If
-    End Sub
+        ws.Range("J" & cnt).Interior.ColorIndex = 3
+        
+    Else
+        
+        ws.Range("J" & cnt).Interior.ColorIndex = 4
+        
+    End If
+    ws.Range("I:I", "L:L").BorderAround LineStyle:=xlContinuous, Weight:=xlThin
+    ws.Range("I:I", "L:L").EntireColumn.AutoFit
+    ws.Range("I1:L1").Interior.ColorIndex = 36
+End Sub
+
+Sub divZero(ws As Worksheet, rowCount As Integer, yrChange As Double, open_price As Double)
     
-    Sub greatCal(ws As Worksheet):
+    If open_price = 0 Then
+        
+        ws.Range("K" & rowCount).Value = 0
+        
+    Else
+        
+        ws.Range("K" & rowCount).Value = Format(yrChange / open_price, "Percent")
+        
+    End If
+End Sub
+
+Sub greatCal(ws     As Worksheet, lastRow As Long):
+    
+    Dim greatest_inc As Double
+    Dim greatest_dec As Double
+    Dim greatest_vol As Double
+    Dim results     As Double
+    
+    Set Rng = Range("I2:L" & lastRow)
     
     ws.Cells(2, 15).Value = "Greatest % increase"
     ws.Cells(3, 15).Value = "Greatest % decrease"
     ws.Cells(4, 15).Value = "Greatest total volume"
     
-    ws.Cells(2, 17).Value = Format(WorksheetFunction.Max(ws.Range("K:K")), "Percent")
-    ws.Cells(3, 17).Value = Format(WorksheetFunction.Min(ws.Range("K:K")), "Percent")
-    ws.Cells(4, 17).Value = WorksheetFunction.Max(ws.Range("L:L"))
+    greatest_inc = WorksheetFunction.Max(ws.Range("K:K"))
+    greatest_dec = WorksheetFunction.Min(ws.Range("K:K"))
+    greatest_vol = WorksheetFunction.Max(ws.Range("L:L"))
+    
+    ws.Cells(2, 16).Value = WorksheetFunction.Index(ws.Range("I:I"), WorksheetFunction.Match(WorksheetFunction.Max(ws.Range("K:K")), ws.Range("K:K"), 0))
+    ws.Cells(3, 16).Value = WorksheetFunction.Index(ws.Range("I:I"), WorksheetFunction.Match(WorksheetFunction.Min(ws.Range("K:K")), ws.Range("K:K"), 0))
+    ws.Cells(4, 16).Value = WorksheetFunction.Index(ws.Range("I:I"), WorksheetFunction.Match(WorksheetFunction.Max(ws.Range("L:L")), ws.Range("L:L"), 0))
+    
+    ws.Cells(2, 17).Value = Format(greatest_inc, "Percent")
+    ws.Cells(3, 17).Value = Format(greatest_dec, "Percent")
+    ws.Cells(4, 17).Value = greatest_vol
     
     ws.Range("O:O", "Q:Q").EntireColumn.AutoFit
     
-    End Sub
-    
+End Sub
